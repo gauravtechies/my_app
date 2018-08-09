06e46b28712b4ea7915a256a08816e8d6ab1579e
Gaurav kamboj <gauravkamboj13121992@gmail.com>
2018-08-09 13:55:04 +0530

    password_confirmation

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.8'
+#bcrypt method for doing encryting the password
+gem 'bcrypt', '~>3.1.7'
 # Bootstrap added 
 gem 'bootstrap-sass', '~> 3.3.5'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   resources :articles
    root 'welcome#home'
    get 'about'=>'welcome#about'
+   get 'signup'=>'users#new'
+   resources :users, expect: [:new]  
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
